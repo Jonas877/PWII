4b3a743229e978f8e8d47493e00610b2e01fc210
jonas <jatobaj70@gmail.com>
2024-08-15 22:30:42 -0300

    Aula de Variáveis

--- variavel.php
+++ variavel.php
@@ -49,7 +49,28 @@
 
    
      <div class="container">
-     <h1>Página inicial do site</h1>
+
+     <h1>Variáveis em PHP</h1>
+     <p> $nunero = 10;</p>
+     <p> $string1 = "Cristiano Ronaldo 7"; </p>
+     <p> $string2= "Cristiano Ronaldo 7" .$numero; </p>
+     <p> $valorDecimal = 10.50; </p>
+     <p> $Ativo = true; </p>
+     <p> /* Aqui é um bloco de comentario */ </p>
+     <?php  //Tudo que está entre as tags do php não aparece na tela?>
+     <?php // a não ser que usemos a função echo com string ?>
+
+     <?php
+     echo "Hello World" ;
+     echo "<br>";
+     $numero = 10;
+     $nome = " Jonas";
+     $frase1 = " <p> $nome camisa $numero frase com paragrafo </p>";
+     $frase2 = $nome. "  camisa  " .$numero; //cocatenação das antigas
+     echo $frase1;
+     echo "$frase2 É o que tem dentro da variavel";
+     ?>
+
      
     <script src="bootstrap.bundle.min.js"></script>
 </body>
