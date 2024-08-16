1c23528931ca6dc2dd0f232f7d675cb6b50d2cf0
Jonas <jatobaj70@gmail.com>
2024-08-16 20:43:38 -0300

    Aula 16-08

--- if.php
+++ if.php
@@ -16,8 +16,10 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="index.php">Página Inicial</a>
+
         </li>
         <li class="nav-item">
           <a class="nav-link" href="variavel.php">Variável</a>
@@ -26,6 +28,7 @@
         <li class="nav-item">
           <a class="nav-link" href="if.php">If</a>
         </li> 
+
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
@@ -49,12 +52,101 @@
   </div>
 </nav>
 
-
-
-   
      <div class="container">
-     <h1>Página inicial do site</h1>
-     
+     <h1>Estrutura Condicional (IF ELSE)</h1>
+
+    <p>$inteira = 10 </p>
+    <p>$nome = "prof" </p>
+    <p>$valor = 10.50 </p>
+    <p>$ativo = true </p>
+
+    
+
+<pre> if($inteira == 10)
+    {
+        echo "o conteudo é 10";
+    }
+    else if($inteira >=11)
+    {
+        echo "não é 10";
+    }
+</pre>
+
+<?php
+    $inteira = 9;
+    if($inteira == 10)
+    {
+        echo "A variavel é 10</h2>";
+    }
+    else if($inteira >= 11)
+    {
+    echo "<h2> A variavel só pode ser menor que 10</h2>";
+    }
+    else 
+    {
+        echo "<h2> A variavel só pode ser menor que 10</h2>";
+    }
+?>
+<p> 
+    As variaveis string podem ser contados os caracteres e usadas no if
+</p>
+<pre>
+    if(strlen($nome) > 10)
+    {
+        echo "seu nome tem mais que 10 caracteres quantidade é " .strlen($nome);
+    }
+</pre>
+<?php
+        $nome = "Fernando";
+        if(strlen($nome) > 5 )
+        {
+            echo "<h2>Seu nome tem mais que 10 caracteres quantidade é " .strlen($nome)."</h2>";
+        }
+       else
+       {
+        echo  "<h2>Seu nome tem mais que 10 caracteres quantidade é " .strlen($nome)."</h2>";
+       }
+
+        ?>
+
+      
+
+
+<p>
+    Podemos verificar se  a variavel $nome está vazia
+</P>
+<pre>
+    if(empty($nome))
+    {
+        echo "Sua variavel está vazia";
+    }
+</pre>
+<p>
+    Também podemos comparar string normalmente
+</P>
+<pre>
+    if($nome == "Fernando")
+    {
+        echo "Nome igual";
+    }
+    else
+    {
+        echo "Nomes diferentes";
+    }
+</pre>
+<p>
+    Podemos verificar se a variavel é verdadeira ou falsa
+</p>
+<pre>
+    if($ativo)
+    {
+        echo "Verdadeira";
+    }
+    else
+    {
+        echo "Falsa";
+    }
+    
     <script src="bootstrap.bundle.min.js"></script>
 </body>
 </html
